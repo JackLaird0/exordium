404cf9584173a79f2ee00fa246357179bd944506
Jack Laird <jack.laird0@gmail.com>
2018-07-21 13:27:53 -0600

    Import HomePage component into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import HomePage from '../HomePage/HomePage'
 import { Route } from 'react-router-dom';
 
 
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="app">
         <Route path='/' component={Header} />
+        <Route exact path='/' component={HomePage} />
       </div>
     );
   }
