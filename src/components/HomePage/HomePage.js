d6d9430789ba4823a0b632f3ca2a957b009376d7
Jack Laird <jack.laird0@gmail.com>
2018-07-24 19:59:18 -0600

    Add content for lesson descriptions

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './HomePage.css';
+import html from '../../assets/html-logo.svg.png';
+import css from '../../assets/logo-css.svg';
+import jsLogo from '../../assets/Javascript-shield.png';
 
 export class HomePage extends Component {
   constructor() {
@@ -10,15 +13,45 @@ export class HomePage extends Component {
     return (
       <main>
         <div className="hero-image">
-          <h3 className="main-text">welcome to the beginning of a new kind of learning </h3>
-          <p className="sub-text">Programming challenges you to use both your creativity and logic</p>
+          <h3 className="main-text">learn the basic skills necessary to build your own website</h3>
+          <p className="sub-text">Programming challenges you to use both creativity and logic</p>
         </div>
         <div className="lessons">
-          <h3 className="lessons-header">Get Started With</h3>
+          <h3 className="lessons-header">Get Started With:</h3>
           <div className="lessons-container">
-            <button className="lesson-link">HTML</button>
-            <button className="lesson-link">CSS</button>
-            <button className="lesson-link">JAVASCRIPT</button>
+            <div className="language-container">
+              <h3 className="language-title">HTML</h3>
+              <div className="lesson-divider"></div>
+              <div className="lesson-description">
+                <div className="logo-container">
+                  <img src={html} alt="HTML Logo" className="lesson-logo"/>
+                  <p className="language-summary"> THE CONTENT</p>
+                </div>
+                <p> But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself </p>
+              </div>
+            </div>
+            <div className="language-container">
+              <h3 className="language-title">CSS</h3>
+              <div className="lesson-divider"> </div>
+              <div className="lesson-description">
+                <div className="logo-container">
+                  <img src={css} alt='CSS Logo' className="lesson-logo css-logo"/>
+                  <p className="language-summary"> THE STYLE</p>
+                </div>
+                <p> But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself </p>
+              </div>
+            </div>
+            <div className="language-container">
+              <h3 className="language-title">JAVASCRIPT</h3>
+              <div className="lesson-divider"> </div>
+              <div className="lesson-description">
+                <div className="logo-container">
+                  <img src={jsLogo} alt="JS Shield" className="lesson-logo"/>
+                  <p className="language-summary"> THE LOGIC</p>
+                </div>
+                <p> But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself </p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="resources">
