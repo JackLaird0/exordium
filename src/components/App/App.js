322c84c190c73f2b6961d839c8654e1b4a538aa9
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-24 20:23:36 -0600

    Configure JSLessonHome component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import HomePage from '../HomePage/HomePage';
-import CssLesson from '../Lessons/CssLesson';
+import CSSLessonHome from '../CSSLessons/CSSLessonHome';
+import JSLessonHome from '../JSLessons/JSLessonHome';
 import { Route } from 'react-router-dom';
 
 
@@ -12,7 +13,8 @@ class App extends Component {
       <div className="app">
         <Route path='/' component={Header} />
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/lessons_css' component={ CssLesson } />
+        <Route exact path='/lessons_css' component={ CSSLessonHome } />
+        <Route exact path='/lessons_js' component={ JSLessonHome } />        
       </div>
     );
   }
---
+++ src/components/JSLessons/JSLessonHome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class JSLessonHome extends Component {
+
+  render() {
+    return (
+      <div> sup </div>
+    )
+  }
+}
+
+export default JSLessonHome;
