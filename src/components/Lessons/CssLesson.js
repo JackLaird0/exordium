df4484ad569733139c1ae3208548978230e5bc97
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-24 19:32:07 -0600

    Create CssLesson component, and corresponding test and css file

---
+++ src/components/Lessons/CssLesson.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './CssLesson.css';
+
+export class CssLesson extends Component {
+  
+  render() {
+    return (
+      <div>
+        Sup
+      </div>
+    )
+  }
+}
+
+export default CssLesson;
