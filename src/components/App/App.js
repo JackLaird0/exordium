dfad90ad4e6b1cd7d55d5362cb750048ff23ed33
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-24 21:14:09 -0600

    Create route for HTMLLessonsHome and populate content

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import Header from '../Header/Header';
 import HomePage from '../HomePage/HomePage';
 import CSSLessonHome from '../CSSLessons/CSSLessonHome';
 import JSLessonHome from '../JSLessons/JSLessonHome';
+import HTMLLessonHome from '../HTMLLessons/HTMLLessonHome'
 import { Route } from 'react-router-dom';
 
 
@@ -14,7 +15,8 @@ class App extends Component {
         <Route path='/' component={Header} />
         <Route exact path='/' component={HomePage} />
         <Route exact path='/lessons_css' component={ CSSLessonHome } />
-        <Route exact path='/lessons_js' component={ JSLessonHome } />        
+        <Route exact path='/lessons_js' component={ JSLessonHome } />  
+        <Route exact path='/lessons_html' component={HTMLLessonHome} />       
       </div>
     );
   }
