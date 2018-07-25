3f8f05535eecc0da5170ec1736e63c30e8e0d940
Jack Laird <jack.laird0@gmail.com>
2018-07-24 23:33:36 -0600

    Style resources section

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,6 +3,9 @@ import './HomePage.css';
 import html from '../../assets/html-logo.svg.png';
 import css from '../../assets/logo-css.svg';
 import jsLogo from '../../assets/Javascript-shield.png';
+import turing from '../../assets/turing-logo.png';
+import mdnLogo from '../../assets/mdn-logo.png';
+import cssTricksLogo from '../../assets/css-tricks-logo.png';
 
 export class HomePage extends Component {
   constructor() {
@@ -14,10 +17,10 @@ export class HomePage extends Component {
       <main>
         <div className="hero-image">
           <h3 className="main-text">learn the basic skills necessary to build your own website</h3>
-          <p className="sub-text">Programming challenges you to use both creativity and logic</p>
+          <p className="sub-text">Programming will challenge you to use both creativity and logic</p>
         </div>
         <div className="lessons">
-          <h3 className="lessons-header">Get Started With:</h3>
+          <h3 className="lessons-header">get started</h3>
           <div className="lessons-container">
             <div className="language-container">
               <h3 className="language-title">HTML</h3>
@@ -55,12 +58,21 @@ export class HomePage extends Component {
           </div>
         </div>
         <div className="resources">
-          <h3 className="resources-header">Check Out These Resources to Continue your Learning</h3>
+          <h3 className="resources-header">-helpful Resources-</h3>
           <div className="resources-container">
-            <p>Turing</p>
-            <p>MDN</p>
-            <p>FlexBox Froggy</p>
-            <p>CSS Tricks</p>
+            <div className="resource">
+              <img src={turing} alt="Turing logo" className="resource-logo"/>
+              <p className="resource-name">TURING SCHOOL</p>
+            </div>
+            <div className="resource">
+              <img src={mdnLogo} alt="mdn logo" className="resource-logo"/>
+              <p className="resource-name">MDN WEB DOCS</p>
+            </div>
+            <div className="resource">
+              <img src={cssTricksLogo} alt="css tricks logo" className="resource-css-logo"/>
+              <p className="resource-name">CSS-TRICKS</p>
+            </div>
+            <p></p>
           </div>
         </div>
       </main>
