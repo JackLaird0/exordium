4a5626a3cbe93683432c6f5c2bacf24b3612f05e
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-26 19:25:34 -0600

    Create html structure for Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
+import './Login.css';
 
 export class Login extends Component {
   constructor() {
     super()
+
+    this.state = {
+      name: '',
+      username: '',
+      password: ''
+    }
   }
 
   render() {
     return (
-      <div>
-        sup
+      <div className='login-container'>
+        <form className='sign-up'>
+          <input
+            type='text'
+            placeholder='Name'
+          />
+          <input
+            type='text'
+            placeholder='Username'
+          />
+          <input
+            type='text'
+            placeholder='Password'
+          />
+          <input
+            type='text'
+            placeholder='Password'
+          />
+        </form>
+        <form className='login'>
+          <input
+            type='text'
+            placeholder='Username'
+          />
+          <input
+            type='text'
+            placeholder='Password'
+          />
+        </form>
       </div>
     )
   }
