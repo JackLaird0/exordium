1c2acdd025558aece46391f9630c9694ae838f94
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-26 19:02:32 -0600

    Create login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        sup
+      </div>
+    )
+  }
+}
+
+export default Login;
