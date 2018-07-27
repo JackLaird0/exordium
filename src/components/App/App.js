bdda93fbd6c5ee533e6c0e178b6d01d07d64374a
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-26 19:02:22 -0600

    Create toggle login function and pass to header as props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,22 @@ import Header from '../Header/Header';
 import HomePage from '../HomePage/HomePage';
 import CSSLessonHome from '../CSSLessons/CSSLessonHome';
 import JSLessonHome from '../JSLessons/JSLessonHome';
-import HTMLLessonHome from '../HTMLLessons/HTMLLessonHome'
+import HTMLLessonHome from '../HTMLLessons/HTMLLessonHome';
+import Login from '../Login/Login';
 import { Route } from 'react-router-dom';
 
 
 class App extends Component {
+
+  toggleLogin() {
+    console.log('woo')
+  }
+
   render() {
     return (
       <div className="app">
-        <Route path='/' component={Header} />
+        <Route path='/' render={() => <Header toggleLogin={this.toggleLogin}/>}/>
+        <Route path='/' render={() => <Login />} />
         <Route exact path='/' component={HomePage} />
         <Route exact path='/lessons_css' component={ CSSLessonHome } />
         <Route exact path='/lessons_js' component={ JSLessonHome } />  
