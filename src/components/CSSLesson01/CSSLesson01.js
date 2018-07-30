da607561282c1213176be664581bf2449b7fc732
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-30 10:12:23 -0600

    Add basic styles to CSSLesson01

--- src/components/CSSLesson01/CSSLesson01.js
+++ src/components/CSSLesson01/CSSLesson01.js
@@ -1,11 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './CSSLesson01.css';
 
 export class CSSLesson01 extends Component {
 
   render() {
     return (
-      <div>
-        sup
+      <div className="css-lesson-01">
+        <div className="game-controls">
+          <h1>Lesson 01: Flex Box</h1>
+          <p>Use your knowledge of flex box to manipulate the box so it is on top of the target.
+          </p>
+          <div className="text-editor">
+            <div className="line-numbers">
+              <p>1</p>
+              <p>2</p>
+              <p>3</p>
+              <p>4</p>
+              <p>5</p>
+              <p>6</p>
+              <p>7</p>
+              <p>8</p>
+              <p>9</p>
+              <p>10</p>              
+            </div>
+            <div className="code">
+              <p>.block &#123;</p>
+              <p>display: flex;</p>
+              <form>
+                <input type="text"/>
+              </form>
+              <p>&#125;</p>
+            </div>
+          </div>
+        </div>
+        <div className="game-display">
+          <div className="box">
+          </div>
+          <div className="target">
+          </div>
+        </div>
       </div>
     )
   }
