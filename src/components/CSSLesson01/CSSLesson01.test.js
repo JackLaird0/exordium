903dcad0feec26342ab6466121ed6a9c83a6bd2f
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-30 19:31:27 -0600

    Delete test in app and write/pass tests for CSSLesson01

--- src/components/CSSLesson01/CSSLesson01.test.js
+++ src/components/CSSLesson01/CSSLesson01.test.js
@@ -13,4 +13,32 @@ describe('CSSLesson01', () => {
   it('matches the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   })
+
+  describe('handleInputChange', () => {
+
+    it('sets the styles property on state to have a key and value that match the input from user', () => {
+      let mockEvent = {target: { value: 'justify-content: flex-end;'}};
+      let expected = {
+        'justify-content': ' flex-end;'
+      }
+      wrapper.instance().handleInputChange(mockEvent);
+      expect(wrapper.state('styles')).toEqual(expected)
+    })
+  })
+
+  describe('toggleNextButton', () => {
+
+    it('sets the nextButton to false if the userInput is correct', () => {
+      let mockEvent = { target: { value: 'justify-content: flex-end;' } };
+
+      wrapper.instance().toggleNextButton(mockEvent)
+      expect(wrapper.state('nextButton')).toEqual(false)
+    })
+
+    it('sets the nextButton to true if the userInput is incorrect', () => {
+      let mockEvent = { target: { value: 'justify-content: garbage' } };
+      wrapper.instance().toggleNextButton(mockEvent)
+      expect(wrapper.state('nextButton')).toEqual(true)      
+    })
+  })
 })
